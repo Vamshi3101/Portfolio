a46606f2dd180d7fb244f752a1750a38694775d9
Vamshi <vamshi3101@gmail.com>
2023-05-31 17:05:43 +0530

    nav bar

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ for(var i = 0;i<document.querySelectorAll(".tab-links").length;i++){
         document.querySelector("#"+this.innerText).classList.add("active-link");
     })
 }
+document.querySelector(".fa-bars").addEventListener("click",()=>{
+    document.querySelector(".nav-links").style.right = "0";
+})
+document.querySelector(".fa-circle-xmark").addEventListener("click",()=>{
+    document.querySelector(".nav-links").style.right = "-100vw";
+})
+for(var i = 0;i<document.querySelectorAll(".nav-li").length;i++){
+    document.querySelectorAll(".nav-li")[i].addEventListener("click",()=>{
+        document.querySelector(".nav-links").style.right = "-100vw";
+    })
+}
